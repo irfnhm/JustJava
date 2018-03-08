9b8452c623378256a7ff53edc0fce012ada4e6b4
Irfan Hamid <irfnhm@gmail.com>
2018-03-08 16:13:01 +0530

    added localization
    Remove hardcoded strings, added translation

--- app/src/main/java/com/example/irfan/justjava/MainActivity.java
+++ app/src/main/java/com/example/irfan/justjava/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.irfan.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -12,6 +17,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 2;
+    int whippedCream = 1;
+    int chocolate = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,16 +50,36 @@ public class MainActivity extends AppCompatActivity {
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
 
+        EditText editTextName = (EditText) findViewById(R.id.name_edit_text);
+        String name = editTextName.getText().toString();
+
+        EditText editTextEmail = (EditText) findViewById(R.id.email_edit_text);
+        String email = editTextEmail.getText().toString();
+
         // Figure out if the user wants chocolate topping
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
 
         // Calculate the price
-        int price = calculatePrice();
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
 
         // Display the order summary on the screen
-        String message = createOrderSummary(price, hasWhippedCream, hasChocolate);
-        displayMessage(message);
+        String message = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
+        //displayMessage(message);
+
+        //disable editext after order is placed.
+        editTextName.setEnabled(false);
+        editTextEmail.setEnabled(false);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, email);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 
     /**
@@ -60,8 +87,20 @@ public class MainActivity extends AppCompatActivity {
      *
      * @return total price
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+        if(hasWhippedCream == true && hasChocolate == true){
+            return ((quantity * 5) + whippedCream + chocolate);
+        }
+        else if(hasWhippedCream == true && hasChocolate == false){
+            return ((quantity * 5) + whippedCream);
+        }
+        else if (hasWhippedCream == false && hasChocolate == true){
+            return ((quantity * 5) + chocolate);
+        }
+        else{
+            return (quantity * 5);
+        }
+
     }
 
     /**
@@ -72,13 +111,13 @@ public class MainActivity extends AppCompatActivity {
      * @param addChocolate    is whether or not to add chocolate to the coffee
      * @return text summary
      */
-    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate) {
-        String priceMessage = "Name: Irfan";
-        priceMessage += "\nAdd whipped cream? " + addWhippedCream;
-        priceMessage += "\nAdd chocolate? " + addChocolate;
-        priceMessage += "\nQuantity: " + quantity;
-        priceMessage += "\nTotal: $" + price;
-        priceMessage += "\nThank you!";
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String name) {
+        String priceMessage = getString(R.string.order_summary_name, name);
+        priceMessage += "\n" + getString(R.string.order_summary_whipped_cream, addWhippedCream);
+        priceMessage += "\n" + getString(R.string.order_summary_chocolate, addChocolate);
+        priceMessage += "\n" + getString(R.string.order_summary_quantity, quantity);
+        priceMessage += "\n" + getString(R.string.order_summary_price, NumberFormat.getCurrencyInstance().format(price));
+        priceMessage += "\n" + getString(R.string.thank_you);
         return priceMessage;
     }
 
@@ -94,9 +133,9 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+//    private void displayMessage(String message) {
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(message);
+//    }
 
 }
